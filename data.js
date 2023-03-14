44e6bd919b69d5fda46745899358eee8fd03760a
anandbaby1998 <118760887+anandbaby1998@users.noreply.github.com>
2023-03-15 00:00:58 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,14 @@
+let songs = [
+    {
+        name: 'Akale',
+        path: 'music/malsong.mp3',
+        artist: 'Shaan Rahman',
+        cover: 'images/cover.png'
+    },
+    {
+        name: 'Seerum Puliyai Parthen',
+        path: 'music/song2.mp3',
+        artist: 'Ashwath',
+        cover: 'images/cover.png'
+    },
+]
